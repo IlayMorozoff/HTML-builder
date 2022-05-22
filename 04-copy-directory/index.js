641ef8e40a4242cc7afd948ae813f7f371f2814f
Ilay Morozov <morozoff1994@yandex.ru>
2022-05-22 23:21:22 +0300

    fix 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,7 +5,13 @@ const path = require('path');
 async function copyDir() {
   try {
     const pathname = path.join(__dirname, 'files-copy');
-    fsPromises.mkdir(pathname, {recursive: true});
+
+    const folderAndFiles = await fsPromises.readdir(path.join(__dirname), { withFileTypes: true });
+
+    if (folderAndFiles.some((item) => !item.isFile() && item.name === 'files-copy')) {
+      await fsPromises.rm(pathname, { recursive: true });
+    }
+    await fsPromises.mkdir(pathname);
   
     const filesIntoFolder = await fsPromises.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
   
