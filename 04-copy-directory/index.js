82ebca9a05bb8260ce4d90c3b998a6a68c7598ed
Ilay Morozov <morozoff1994@yandex.ru>
2022-05-22 23:02:52 +0300

    added 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+  try {
+    const pathname = path.join(__dirname, 'files-copy');
+    fsPromises.mkdir(pathname, {recursive: true});
+  
+    const filesIntoFolder = await fsPromises.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
+  
+    for (let item of filesIntoFolder) {
+      const rs = fs.createReadStream(path.join(__dirname, 'files', item.name));
+      const ws = fs.createWriteStream(path.join(pathname, item.name));
+      rs.pipe(ws);
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+copyDir();
