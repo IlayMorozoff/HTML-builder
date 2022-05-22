61b651a2b4e35374cbf86a59f8af3fb6f81a594a
Ilay Morozov <morozoff1994@yandex.ru>
2022-05-23 00:00:12 +0300

    5 task completed

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+async function mergeStyles() {
+  try {
+    const pathname = path.join(__dirname, 'project-dist');
+
+    const filesIntoFolder = await fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
+
+    fs.promises.writeFile(path.join(pathname, 'bundle.css'), '');
+  
+    for (let item of filesIntoFolder) {
+      if (path.parse(item.name).ext === '.css') {
+        const rf = await fs.promises.readFile(path.join(__dirname, 'styles', item.name), {encoding: 'utf8'});
+        await fs.promises.appendFile(path.join(pathname, 'bundle.css'), rf);
+      }
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+mergeStyles();
